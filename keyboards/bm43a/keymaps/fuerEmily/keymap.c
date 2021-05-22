6b8e3957e04b6bfc4883ba03c5e26746c89b6958
Signynt <67801159+Signynt@users.noreply.github.com>
2021-05-22 13:00:27 +0200

    updated keymap

--- keyboards/bm43a/keymaps/fuerEmily/keymap.c
+++ keyboards/bm43a/keymaps/fuerEmily/keymap.c
@@ -1,17 +1,4 @@
 /* qmk flash -kb bm43a -km fuerEmily
- *
- * This program is free software: you can redistribute itqwe  aaeappwawww aqwq qwwwqqqqqqqq q qwwqwqeeqwqw and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
 
@@ -22,20 +9,26 @@
 #define KC_SAI RGB_SAI
 #define KC_VAI RGB_VAI
 
-enum layers {BASE, LAYER1};
+enum layers {BASE, LAYER1, LAYER2};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,
-        KC_LGUI, KC_LALT, KC_LALT,          KC_SPC       ,             LT(LAYER1, KC_SPC)       ,KC_NO,    KC_LEFT, KC_DOWN, KC_QUOT
+        KC_LSFT,          KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,
+        KC_LCTL, KC_LALT, KC_LGUI,   LT(LAYER2, KC_SPC)  ,    LT(LAYER1, KC_SPC)    ,KC_NO,C(KC_SPC), KC_DOWN, KC_QUOT
     ),
     [LAYER1] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,   KC_LEFT,KC_DOWN, KC_UP,   KC_RIGHT,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
-        KC_LSFT,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DOT,  KC_SLASH,
-        KC_LGUI, KC_LALT, KC_LALT,          KC_SPC       ,             KC_SPC       ,KC_NO,    KC_LEFT, KC_DOWN, KC_RST
+        KC_TAB,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC, KC_AMPR,KC_ASTR,  KC_COLN, KC_ENT,
+        KC_LSFT,          KC_PLUS, KC_MINS, KC_EQL,  KC_UNDS, KC_LCBR, KC_RCBR, KC_M,    KC_LPRN, KC_RPRN, KC_SLASH,
+        KC_LCTL, KC_LALT, KC_LGUI,   LT(LAYER2, KC_SPC)  ,    LT(LAYER1, KC_SPC)    ,KC_NO,KC_LEFT, KC_DOWN, KC_QUOT
+    ),
+    [LAYER2] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,
+        KC_TAB,    KC_TOG,  KC_MOD,    KC_HUI,    KC_SAI,    KC_VAI,    KC_H,    KC_VOLU,    KC_K,    KC_L,    KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_MPRV, KC_VOLD,    KC_MNXT,  KC_COMM, KC_UP,  KC_SLASH,
+        KC_RST, KC_LALT, KC_LGUI,   LT(LAYER2, KC_SPC)   ,            KC_MPLY       ,KC_NO,KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
 };
