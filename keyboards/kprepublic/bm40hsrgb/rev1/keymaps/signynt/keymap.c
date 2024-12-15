62f6b89cf8b3a9525a27a1fd9fdc76111a3f4436
Signynt <67801159+Signynt@users.noreply.github.com>
2024-12-15 11:45:58 +0100

    Updated gaming layer

--- keyboards/kprepublic/bm40hsrgb/rev1/keymaps/signynt/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev1/keymaps/signynt/keymap.c
@@ -231,25 +231,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [GAME] = LAYOUT_planck_mit(
   // ╭───────────────────────────────────────────────────────────────────────────────────────────────────────╮
-         KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_NO,   KC_NO,    KC_Y,   KC_U,   KC_I,   KC_O,TG(GAME),
+         KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,    KC_Y,     KC_U,   KC_I,   KC_O,   KC_P,TG(GAME),
   // ├───────────────────────────────────────────────────────────────────────────────────────────────────────┤
-        KC_LSFT,   KC_A,   KC_S,   KC_D,   KC_F,   KC_NO,   KC_NO,    KC_H,   KC_J,   KC_K,   KC_L,KC_QUOT,
+        KC_LSFT,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,    KC_H,     KC_J,   KC_K,   KC_L, KC_COL,KC_QUOT,
   // ├───────────────────────────────────────────────────────────────────────────────────────────────────────┤
-        KC_LCTL,   KC_X,   KC_Z,   KC_C,   KC_V,   KC_NO,   KC_NO,    KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,
+        KC_LCTL,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    KC_N,     KC_M,KC_COMM, KC_DOT,  KC_UP,KC_RCTRL,
   // ├───────────────────────────────────────────────────────────────────────────────────────────────────────┤
-          KC_NO,  KC_NO, KC_TAB, KC_SPC,OSL(SECGAME),   KC_NO,      KC_ENT,KC_BSPC,  KC_NO,  KC_NO,  KC_NO
+           KC_1,   KC_2,   KC_3, KC_TAB, KC_SPC,   OSL(SECGAME),    KC_ENT,KC_BSPC,KC_LEFT,KC_DOWN,KC_RIGHT
   // ╰───────────────────────────────────────────────────────────────────────────────────────────────────────╯
   ),
 
   [SECGAME] = LAYOUT_planck_mit(
   // ╭───────────────────────────────────────────────────────────────────────────────────────────────────────╮
-         KC_ESC,   KC_7,   KC_8,   KC_9,   KC_T,   KC_NO,   KC_NO,    KC_Y,   KC_U,   KC_I,   KC_O,TG(GAME),
+         KC_ESC,   KC_7,   KC_8,   KC_9,   KC_T,   KC_NO,   KC_NO,    KC_Y,   KC_U,  KC_F7,  KC_F8,  KC_F9,
   // ├───────────────────────────────────────────────────────────────────────────────────────────────────────┤
-        KC_LSFT,   KC_4,   KC_5,   KC_6,   KC_G,   KC_NO,   KC_NO,    KC_H,   KC_J,   KC_K,   KC_L,KC_QUOT,
+        KC_LSFT,   KC_4,   KC_5,   KC_6,   KC_G,   KC_NO,   KC_NO,    KC_H,   KC_J,  KC_F4,  KC_F5,  KC_F6,
   // ├───────────────────────────────────────────────────────────────────────────────────────────────────────┤
-        KC_LCTL,   KC_1,   KC_2,   KC_3,   KC_B,   KC_NO,   KC_NO,    KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,
+        KC_LCTL,   KC_1,   KC_2,   KC_3,   KC_B,   KC_NO,   KC_NO,    KC_N,   KC_M,  KC_F1,  KC_F2,  KC_F3,
   // ├───────────────────────────────────────────────────────────────────────────────────────────────────────┤
-          KC_NO,  KC_NO, KC_TAB,   KC_0,  KC_NO,       KC_NO,       KC_ENT,KC_BSPC,  KC_NO,  KC_NO,  KC_NO
+          KC_NO,  KC_NO, KC_TAB,   KC_0,  KC_NO,       KC_NO,       KC_ENT,KC_BSPC, KC_F10,  KC_NO,  KC_NO
   // ╰───────────────────────────────────────────────────────────────────────────────────────────────────────╯
   ),
 };
